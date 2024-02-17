360485ded710ba6625d465f0f71fc9c0fe38f44e
sainathhiwale <sainathhiwale2@gmail.com>
2024-02-17 19:32:48 +0530

    init commit - MVVM, Kotlin Flow, Dagger Hilt, Glide ,Kotlin Coroutines

---
+++ app/src/main/java/com/examen/shoppingapp/ShoppingApplication.kt
@@ -0,0 +1,10 @@
+package com.examen.shoppingapp
+
+import android.app.Application
+
+class ShoppingApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/examen/shoppingapp/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.examen.shoppingapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.examen.shoppingapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
