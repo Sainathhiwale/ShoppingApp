c1fb7913088aba79fb4501b17ef01c517611a058
sainathhiwale <sainathhiwale2@gmail.com>
2024-02-22 22:26:09 +0530

    test-2

--- app/src/main/java/com/examen/shoppingapp/view/SignUpActivity.kt
+++ app/src/main/java/com/examen/shoppingapp/view/SignUpActivity.kt
@@ -2,15 +2,30 @@ package com.examen.shoppingapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.examen.shoppingapp.R
 import com.examen.shoppingapp.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var activitySignUpBinding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activitySignUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(activitySignUpBinding.root)
-        //setContentView(R.layout.activity_sign_up)
+        initView()
+    }
+
+    private fun initView() {
+        activitySignUpBinding.registerButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.register_button ->{
+                val toast = Toast.makeText(this, "clicked", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 }
