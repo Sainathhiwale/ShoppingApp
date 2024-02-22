905d1b02610037cea8f20f4dbdcb2c27bc12ec4b
sainathhiwale <sainathhiwale2@gmail.com>
2024-02-22 21:57:29 +0530

    SignUp data binding is added

--- app/src/main/java/com/examen/shoppingapp/view/SignUpActivity.kt
+++ app/src/main/java/com/examen/shoppingapp/view/SignUpActivity.kt
@@ -3,10 +3,14 @@ package com.examen.shoppingapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.examen.shoppingapp.R
+import com.examen.shoppingapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var activitySignUpBinding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        activitySignUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(activitySignUpBinding.root)
+        //setContentView(R.layout.activity_sign_up)
     }
 }
